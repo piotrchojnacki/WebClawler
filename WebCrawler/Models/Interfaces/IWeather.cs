76ee4a39ade6303739a032da82ec8986af288a0b
piotrchojnacki <piotr.t.chojnacki@gmail.com>
2018-06-28 23:40:26 +0200

    [+][piotrchojnacki] Weather Model

---
+++ WebCrawler/Models/Interfaces/IWeather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Models.Interfaces
+{
+    public interface IWeather
+    {
+        decimal Temp { get; }
+        int Pressure { get; }
+        int Humidity { get; }
+        decimal TempMin { get; }
+        decimal TempMax { get; }
+        int Visibility { get; }
+        decimal WindSpeed { get; }
+        decimal WindDeg { get; }
+        int CloudsAll { get; }
+        long Dt { get; }
+    }
+}
---
+++ WebCrawler/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Models.Interfaces;
+
+namespace WebCrawler.Models
+{
+    public class Weather : IWeather
+    {
+        public decimal Temp { get; private set; }
+        public int Pressure { get; private set; }
+        public int Humidity { get; private set; }
+        public decimal TempMin { get; private set; }
+        public decimal TempMax { get; private set; }
+        public int Visibility  { get; private set; }
+        public decimal WindSpeed { get; private set; }
+        public decimal WindDeg { get; private set; }
+        public int CloudsAll { get; private set; }
+        public long Dt { get; private set; }
+    }
+}
